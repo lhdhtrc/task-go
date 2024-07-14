ba9995c7f94ca0866b8ca7c63f2d594b13b572fd
lhdht <15229684090@163.com>
2024-07-15 07:43:05 +0800

    perfect: 迁移

--- pkg/internal.go
+++ pkg/internal.go
@@ -5,84 +5,93 @@ import (
 	"time"
 )
 
+func (core *CoreEntity) install() {
+	// 启动初始数量的routine
+	for i := int32(0); i < core.MinConcurrency; i++ {
+		core.addRoutine()
+	}
+
+	go core.monitorRoutine()
+}
+
 // monitorQueue 监控任务队列长度并动态调整worker数量
-func (s *CoreEntity) monitorTask() {
+func (core *CoreEntity) monitorRoutine() {
 	ticker := time.NewTicker(time.Second) // 每秒检查一次任务队列长度并调整worker数量
 	defer ticker.Stop()
 
 	for {
 		select {
-		case <-s.ctx.Done():
+		case <-core.ctx.Done():
 			return
 		case <-ticker.C:
-			l := len(s.queue)
-			i := atomic.LoadInt32(&s.routineCount) - int32(l)
-			if l > 0 && i < s.MaxConcurrency-s.MinConcurrency {
-				s.addRoutine()
+			l := len(core.queue)
+			i := atomic.LoadInt32(&core.routineCount) - int32(l)
+			if l > 0 && i < core.MaxConcurrency-core.MinConcurrency {
+				core.addRoutine()
 			} else if i > 0 {
-				s.removeRoutine()
+				core.removeRoutine()
 			}
 		}
 	}
 }
 
-func (s *CoreEntity) addRoutine() {
-	s.mu.Lock()
-	defer s.mu.Unlock()
+func (core *CoreEntity) addRoutine() {
+	core.mu.Lock()
+	defer core.mu.Unlock()
 
-	if atomic.LoadInt32(&s.routineCount) >= s.MaxConcurrency {
+	if atomic.LoadInt32(&core.routineCount) >= core.MaxConcurrency {
 		// 如果routine数量已经达到最大值，则不添加
 		return
 	}
 
 	// 增加routine计数，然后启动routine
-	atomic.AddInt32(&s.routineCount, 1)
-	s.wg.Add(1)
-	go s.routine()
+	atomic.AddInt32(&core.routineCount, 1)
+	core.wg.Add(1)
+	go core.routine()
 
-	if s.withAddRoutine != nil {
-		s.withAddRoutine()
+	if core.withAddRoutine != nil {
+		core.withAddRoutine()
 	}
 }
 
-func (s *CoreEntity) removeRoutine() {
-	s.mu.Lock()
-	defer s.mu.Unlock()
+func (core *CoreEntity) removeRoutine() {
+	core.mu.Lock()
+	defer core.mu.Unlock()
 
-	if atomic.LoadInt32(&s.routineCount) <= s.MinConcurrency {
+	if atomic.LoadInt32(&core.routineCount) <= core.MinConcurrency {
 		// 如果routine数量已经达到最小值，则不移除
 		return
 	}
 
 	// 尝试发送停止信号给routine
 	select {
-	case s.stop <- 1:
-		if s.withRemoveRoutine != nil {
-			s.withRemoveRoutine()
+	case core.stop <- 1:
+		if core.withRemoveRoutine != nil {
+			core.withRemoveRoutine()
 		}
 	default:
 		// 没有routine可移除，routine处于忙碌状态
 	}
 }
 
-func (s *CoreEntity) routine() {
-	defer atomic.AddInt32(&s.routineCount, -1) // 无论任务是否完成，routine退出时都减少计数
+func (core *CoreEntity) routine() {
+	defer atomic.AddInt32(&core.routineCount, -1) // 无论任务是否完成，routine退出时都减少计数
 
 	for {
 		select {
-		case task := <-s.queue:
+		case task := <-core.queue:
 			// 执行任务
 			st := time.Now()
 			te := task.Handle()
 			et := time.Now()
 			dt := et.Sub(st)
-			if s.withRunTask != nil {
-				s.withRunTask(task.Id, dt, te)
+			if core.withRunTask != nil {
+				core.withRunTask(task.Id, dt, te)
 			}
-		case <-s.stop:
+		case <-core.stop:
 			// 收到信号，routine终止
 			return
-		case <-s.ctx.Done():
+		case <-core.ctx.Done():
 			// 池被关闭，routine退出
 			return
 		}
