e133324d4d057db5a48883cb59b7c38ded906470
lhdht <15229684090@163.com>
2024-07-15 07:42:52 +0800

    perfect: 迁移

---
+++ pkg/core.go
@@ -0,0 +1,63 @@
+package task
+
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+func New(config ConfigEntity) *CoreEntity {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	core := &CoreEntity{
+		ConfigEntity: config,
+
+		queue:  make(chan RawEntity, config.MaxCache),
+		stop:   make(chan int),
+		ctx:    ctx,
+		cancel: cancel,
+	}
+
+	core.install()
+
+	return core
+}
+
+func (core *CoreEntity) Add(task RawEntity) {
+	select {
+	case core.queue <- task:
+		// 任务成功提交到队列
+		if core.withAddTaskSuccess != nil {
+			core.withAddTaskSuccess(task.Id)
+		}
+	default:
+		// 队列已满，可以选择丢弃任务或进行其他处理
+		if core.withAddTaskError != nil {
+			core.withAddTaskError(errors.New("queue is full, job was not submitted"))
+		}
+	}
+}
+
+func (core *CoreEntity) Await(second int64) {
+	for {
+		if len(core.queue) == 0 {
+			time.Sleep(time.Duration(second) * time.Second)
+			break
+		}
+	}
+}
+
+func (core *CoreEntity) Count() int {
+	return len(core.queue)
+}
+
+func (core *CoreEntity) RoutineCount() int32 {
+	return core.routineCount
+}
+
+func (core *CoreEntity) Uninstall() {
+	core.cancel()     // 取消上下文，通知所有routine停止工作
+	close(core.stop)  // 发送信号到所有routine
+	core.wg.Wait()    // 等待所有routine退出
+	close(core.queue) // 关闭任务队列
+}
